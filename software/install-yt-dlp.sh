86235284d76e0d7276404465a78425593b4a2366
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 15:30:59 +0200

    Update install-yt-dlp.sh

--- software/install-yt-dlp.sh
+++ software/install-yt-dlp.sh
@@ -9,6 +9,6 @@ echo
 if ! location=$(type -p "yt-dlp"); then
     sudo apt install -y ffmpeg
 
-    sudo wget -O /usr/local/bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/download/2022.03.08.1/yt-dlp
+    sudo wget -O /usr/local/bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
     sudo chmod a+rx /usr/local/bin/yt-dlp
 fi
