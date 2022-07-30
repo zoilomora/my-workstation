3c86900b35912fff3867fe733d38e7a56da856dc
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 15:13:15 +0200

    Update install-google-chrome.sh

--- software/install-google-chrome.sh
+++ software/install-google-chrome.sh
@@ -8,6 +8,7 @@ echo
 
 repositories=$(grep ^[^#] /etc/apt/sources.list /etc/apt/sources.list.d/*)
 if ! repository=$(echo "$repositories" | grep "dl.google.com"); then
+    sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /etc/apt/keyrings/google-chrome.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
     sudo apt update
