9895ad7b46a8d03cceaecca54b1ed1f857751196
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 13:41:53 +0200

    Update uninstall-applications-by-default.sh

--- software/uninstall-applications-by-default.sh
+++ software/uninstall-applications-by-default.sh
@@ -6,15 +6,48 @@ echo "  Uninstalling Applications by Default                          "
 echo "################################################################"
 echo
 
+# Gnome
 sudo apt remove -y \
+    gnome-2048 \
     gnome-calendar \
-    gnome-mines \
+    gnome-chess \
+    gnome-clocks \
+    gnome-contacts \
+    gnome-documents \
+    gnome-klotski \
     gnome-mahjongg \
+    gnome-maps \
+    gnome-mines \
+    gnome-music \
+    gnome-nibbles \
+    gnome-robots \
     gnome-sudoku \
-    aisleriot \
-    simple-scan \
-    rhythmbox \
-    totem \
+    gnome-taquin \
+    gnome-tetravex \
+    gnome-todo \
+    gnome-weather
+
+# Apps
+sudo apt remove -y \
     cheese \
+    evolution \
+    libreoffice-calc \
+    libreoffice-draw \
+    libreoffice-impress \
+    libreoffice-writer \
+    malcontent \
+    rhythmbox \
     shotwell \
-    gnome-todo
+    totem \
+    transmission-gtk
+
+# Games
+sudo apt remove -y \
+    five-or-more \
+    four-in-a-row \
+    hitori \
+    iagno \
+    lightsoff \
+    quadrapassel \
+    swell-foop \
+    tali
