6c3459c58beda88bc2f4d9185f8d18ce4653edae
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 15:21:47 +0200

    Update install-visual-studio-code.sh

--- software/install-visual-studio-code.sh
+++ software/install-visual-studio-code.sh
@@ -8,8 +8,9 @@ echo
 
 repositories=$(grep ^[^#] /etc/apt/sources.list /etc/apt/sources.list.d/*)
 if ! repository=$(echo "$repositories" | grep "packages.microsoft.com"); then
-    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/vscode-archive-keyring.gpg
-    echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /etc/apt/keyrings/vscode.gpg
+    echo deb [arch=amd64 signed-by=/etc/apt/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main | sudo tee /etc/apt/sources.list.d/vscode.list
     sudo apt update
 fi
 
