4601df4110f50f6727ca7637d59390f2a0210891
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 15:14:41 +0200

    Update install-docker.sh

--- software/install-docker.sh
+++ software/install-docker.sh
@@ -12,9 +12,7 @@ repositories=$(grep ^[^#] /etc/apt/sources.list /etc/apt/sources.list.d/*)
 if ! repository=$(echo "$repositories" | grep "download.docker.com"); then
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
 fi
 
