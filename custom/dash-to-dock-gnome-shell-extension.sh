89f1e708bead26a7f613a66bc657002ca8bf1824
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-30 17:54:40 +0200

    Create dash-to-dock-gnome-shell-extension.sh

---
+++ custom/dash-to-dock-gnome-shell-extension.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+echo "################################################################"
+echo "  Installing Dash to Dock GNOME Shell Extension                 "
+echo "################################################################"
+echo
+
+DASHTODOCK_UUID="dash-to-dock@micxgx.gmail.com"
+DASHTODOCK_VERSION="69"
+
+if ! [ -a "~/.local/share/gnome-shell/extensions/$GTILE_UUID" ]; then
+    wget -O /tmp/dash-to-dockmicxgx.gmail.com.v$DASHTODOCK_VERSION.shell-extension.zip https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v$DASHTODOCK_VERSION.shell-extension.zip
+    mkdir -p ~/.local/share/gnome-shell/extensions/$DASHTODOCK_UUID
+    unzip -q /tmp/dash-to-dockmicxgx.gmail.com.v$DASHTODOCK_VERSION.shell-extension.zip -d ~/.local/share/gnome-shell/extensions/$DASHTODOCK_UUID
+    rm /tmp/dash-to-dockmicxgx.gmail.com.v$DASHTODOCK_VERSION.shell-extension.zip
+    #gnome-extensions enable $DASHTODOCK_UUID
+fi
