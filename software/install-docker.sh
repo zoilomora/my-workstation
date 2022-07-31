da602caf892ea47af73e1f5caf5678b935b1f945
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-31 17:02:42 +0200

    Update install-docker.sh

--- software/install-docker.sh
+++ software/install-docker.sh
@@ -11,9 +11,14 @@ sudo apt install -y ca-certificates curl gnupg lsb-release
 repositories=$(grep ^[^#] /etc/apt/sources.list /etc/apt/sources.list.d/*)
 if ! repository=$(echo "$repositories" | grep "download.docker.com"); then
     sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
+fi
+
+if ! [ -a "/bin/docker-compose" ]; then
+    echo 'docker compose "$@"' | sudo tee /bin/docker-compose > /dev/null
+    sudo chmod +x /bin/docker-compose
 fi
 
 if ! location=$(type -p "docker"); then
