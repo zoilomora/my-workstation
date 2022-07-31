f7e4e37e43c94bdfdfcd1b3d1e7f67b4228756de
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-31 17:30:46 +0200

    ICE - SSB Manager

--- software/install-ice.sh
+++ software/install-ice.sh
@@ -1,6 +1,22 @@
-# ICE - SSB Manager
-https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_5.3.0_all.deb
-https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_6.0.7_all.deb
+#!/bin/bash
 
-apt install chromium
-ln -s /usr/bin/chromium /usr/bin/chromium-browser
+echo
+echo "################################################################"
+echo "  Installing ICE - SSB Manager                                  "
+echo "################################################################"
+echo
+
+ICE_VERSION="6.0.7"
+# ICE_VERSION="5.3.0"
+
+if ! location=$(type -p "ice"); then
+    wget -O /tmp/ice_"$ICE_VERSION"_all.deb \
+      https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_"$ICE_VERSION"_all.deb
+    sudo dpkg -i /tmp/ice_"$ICE_VERSION"_all.deb
+    rm /tmp/ice_"$ICE_VERSION"_all.deb
+fi
+
+if ! location=$(type -p "chromium"); then
+  	sudo apt install -y chromium
+  	sudo ln -s /usr/bin/chromium /usr/bin/chromium-browser
+fi
