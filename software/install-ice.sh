304248388a7e5117f1c9038551bb960568cc4057
Zoilo Mora <zoilo.mora@hotmail.com>
2022-07-31 02:18:33 +0200

    Create install-ice.sh

---
+++ software/install-ice.sh
@@ -0,0 +1,6 @@
+# ICE - SSB Manager
+https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_5.3.0_all.deb
+https://launchpad.net/~peppermintos/+archive/ubuntu/ice-dev/+files/ice_6.0.7_all.deb
+
+apt install chromium
+ln -s /usr/bin/chromium /usr/bin/chromium-browser
